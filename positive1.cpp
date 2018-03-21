ad40f50c9fff26d2c0a88369d7b280143e9309e0
MonalisaKoley <36394866+MonalisaKoley@users.noreply.github.com>
2018-03-21 16:23:07 +0530

    Create positive1.cpp

---
+++ positive1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int n;
+ 
+ printf ("Enter a number \n");
+ scanf ("%d", &n);
+ 
+ if (n > 0)
+ {
+ 
+  printf ("You have entered positive number");
+ 
+ } else if (n < 0)
+ {
+ 
+  printf ("You have entered negative number");
+ 
+ } else 
+ {
+ 
+  printf ("You have entered zero");
+ }
+ 
+	// your code goes here
+	return 0;
+}
